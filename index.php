190a1d0784e25076f3bdea3470ebb62f04002ff1
PrashantRH <prashant.rh007@gmail.com>
2020-08-24 11:37:45 +0530

    added required condition

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
             <label>Phone Number</label>
             <input type="tel" name="phone" minlength="10" maxlength="10" required placeholder="Phone number" class="form-control" />
             <label>Email</label>
-            <input type="text" name="email" placeholder="exmple@abc.com" required class="form-control"/><br>
+            <input type="email" name="email" placeholder="exmple@abc.com" required class="form-control"/><br>
             <label>Message</label>
             <textarea name="message" id="" cols="30" rows="10" placeholder="Add Message Here" class="form-control" ></textarea><br>
             <button type="submit" class="btn btn-success">Submit</button>
